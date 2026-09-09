ff3eac1ae7a7d841776a99f8d03c597b163f7265
DimaSh888 <dimon.shipicin@gmail.com>
2026-09-09 18:16:52 +0300

    Update urls.py

--- portal_group/urls.py
+++ portal_group/urls.py
@@ -3,4 +3,9 @@ from portal_group import views
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='main'),
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('profile/', profile, name='profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
+    path('logout/', logout_view, name='logout'),
 ]
